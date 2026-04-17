548fb99464fbbe57e770399738f9d80f2b47383a
Ishan <ishaan10904@gmail.com>
2026-04-17 13:20:13 +0530

    adv

---
+++ loops/la.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+
+int main() {
+    vector<int> arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+
+    int maxSum = INT_MIN, currSum = 0;
+
+    for (int i = 0; i < arr.size(); i++) {
+        currSum += arr[i];
+        maxSum = max(maxSum, currSum);
+        if (currSum < 0) currSum = 0;
+    }
+
+    cout << maxSum;
+
+    return 0;
+}

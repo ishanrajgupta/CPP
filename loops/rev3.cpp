93d8d7deeeb60fb7716273c94bb0281eef2cbca5
Ishan <ishaan10904@gmail.com>
2026-04-19 23:14:26 +0530

    rev day 2 2

---
+++ loops/rev3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;   
+    unsigned long long factorial = 1;
+    for (int i = 1; i <= n; ++i) {
+        factorial *= i;
+    }
+    cout << "Factorial of " << n << " = " << factorial;
+    return 0;
+}
